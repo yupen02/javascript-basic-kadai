3726655e064d7d5a8d886dd7ea22050e70757cc4
yupen02 <133780174+yupen02@users.noreply.github.com>
2023-06-27 21:14:26 +0900

    課題７を再アップロード

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,6 +1,9 @@
-let num = Math.floor(Math.random() * 20)
+let num = 1 + Math.floor(+ Math.random() * 20)
 
-if(num % 3 == 0){
+if(num % 3 == 0 && num % 5 == 0){
+  console.log('３と５の倍数です')
+}
+else if(num % 3 == 0){
   console.log('３の倍数です')
 }
 else if(num % 5 == 0){
